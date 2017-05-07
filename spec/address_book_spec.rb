c7024b681be8057e58f3bdd6dc85db856bcfae3e
compostbrain <joel@wildmeadowsfarm.com>
2017-05-07 12:49:44 -0400

    6-Reading-CSVs-assignment completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -37,6 +37,7 @@ RSpec.describe AddressBook do
 
   # Test that AddressBook's .import_from_csv() method is working as expected
   describe "#import_from_csv" do
+
     it "imports the correct number of entries" do
       book.import_from_csv("entries.csv")
       book_size = book.entries.size
@@ -75,6 +76,32 @@ RSpec.describe AddressBook do
       # Check the fifth entry
       entry_five = book.entries[4]
       check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
+    end
+
+    it "imports the correct number of entries from a second csv file" do
+      book.import_from_csv("entries_2.csv")
+      book_size = book.entries.size
+      expect(book_size).to eq 3
+    end
+
+    it 'imports the 1st entry' do
+      book.import_from_csv('entries_2.csv')
+      entry_one = book.entries[0]
+      check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+    end
+
+    it "imports the 2nd entry" do
+      book.import_from_csv("entries_2.csv")
+      # Check the second entry
+      entry_two = book.entries[1]
+      check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+    end
+
+    it "imports the 3rd entry" do
+      book.import_from_csv("entries_2.csv")
+      # Check the third entry
+      entry_three = book.entries[2]
+      check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
     end
   end
 
